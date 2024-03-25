947ef25a27671978310035bfe3be1ba1a6fd3300
Duc-Lee Banh <duclee.banh@gmail.com>
2024-03-25 12:59:49 +0100

    Added async function to log user

--- FrontEnd/scripts/callAPI.js
+++ FrontEnd/scripts/callAPI.js
@@ -3,7 +3,24 @@ const url = "http://localhost:5678/api";
 /* Async Function fetching Works from API */
 export async function GetWorks () {
     const workList = await fetch(url + "/works");
-    const reponse = await workList.json();
+    const response = await workList.json();
 
-    return reponse;
+    return response;
+}
+
+/* Async Function posting User info to connect */
+export async function PostUser (email, password) {
+    const user = await fetch(url + "/users/login",{
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({"email": email, "password": password})
+    });
+
+    const response = await user.json();
+
+    if (user.status !== 200) {
+        throw new Error(response.message);
+    }
+
+    return response;
 }
