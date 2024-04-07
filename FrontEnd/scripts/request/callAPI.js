4b68b4c933459266cefa582687abef26c88c4723
Duc-Lee Banh <duclee.banh@gmail.com>
2024-04-07 23:04:48 +0200

    Added delete and post request for works

---
+++ FrontEnd/scripts/request/callAPI.js
@@ -0,0 +1,70 @@
+const url = "http://localhost:5678/api";
+
+/* Async Function fetching Works from API */
+export async function GetWorks () {
+    const workList = await fetch(url + "/works");
+    const response = await workList.json();
+
+    return response;
+}
+
+/* Async Function posting User info to connect */
+export async function PostUser (email, password) {
+    const user = await fetch(url + "/users/login",
+    {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({"email": email, "password": password})
+    });
+
+    const response = await user.json();
+
+    if (user.status !== 200) {
+        throw new Error("API Error");
+    }
+
+    return response;
+}
+
+/* Async Function fetching Categories from API */
+export async function GetCategories () {
+    const categoryList = await fetch(url + "/categories");
+    const response = await categoryList.json();
+
+    return response;
+}
+
+/* Async Function deleting Work from API */
+export async function DeleteWork(id) {
+    const work = await fetch(url + "/works/" + id, 
+    {
+        method: "DELETE",
+        headers: {"Authorization": "Bearer " + JSON.parse(window.localStorage.getItem("user")).token}
+    });
+
+    const response = await work.json();
+
+    if (work.status !== 200) {
+        throw new Error("API Error");
+    }
+
+    return response;
+}
+
+export async function PostWork (image, title, categoryId) {
+    const formData = new FormData();
+    formData.append('image', image.files[0]);
+    formData.append('title', title.value);
+    formData.append('category', categoryId.value);
+
+    const work = await fetch(url + "/works",
+    {
+        method: "POST",
+        headers: {"Authorization": "Bearer " + JSON.parse(window.localStorage.getItem("user")).token},
+        body: formData
+    });
+
+    const response = await work.json();
+
+    return response;
+}
